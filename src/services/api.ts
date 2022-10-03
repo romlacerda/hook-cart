7322c174770e3ab1dabb3ae8bbfe973f1008a237
Romulo <rlcrweb@gmail.com>
2022-10-03 20:57:51 -0300

    feat: cart

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,13 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+interface Product {
+  id: string;
+}
+
+export function getProducts() { 
+  return api.get<Product[]>('/products').then(response => console.log(response))
+};
